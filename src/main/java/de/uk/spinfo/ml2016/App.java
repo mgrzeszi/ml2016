35ef1e778a88412bb10fb5a030dad010c51295fb
Philip Schlusslicht <me@phil.re>
2016-11-14 12:33:32 +0100

    Initial commit

---
+++ src/main/java/de/uk/spinfo/ml2016/App.java
@@ -0,0 +1,13 @@
+package de.uk.spinfo.ml2016;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
